78954e4c38bfcd3866c1ee66cac805e552e3a467
Shankha <ribhumaster@gmail.com>
2023-04-20 00:54:53 +0530

    orders, customer and employees page done

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employees = () => {
-  return <div>Employees</div>;
+  const editing = { allowDeleting: true, allowEditing: true };
+
+  return (
+    <div className="p-2 m-2 bg-white md:m-10 md:p-10 rounded-3xl">
+      <Header title="Employees" category="Page" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={["Search"]}
+        editSettings={editing}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
